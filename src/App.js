a5177c1adeb515bff21417c2424ecc66e6b807f9
wangyafei <3218566389@qq.com>
2019-03-28 22:17:11 +0800

    最基础的JSX语法

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
+// 定义一个类
+// 这个类继承了react下的Component
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-      hello world
-      </div>
-    );
-  }
+    render() {
+        // JSX
+        return (
+            <div className="App">
+                hello world
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 // all in js 在react中css和js可以通过模块的形式嵌入到react中
 // import './index.css';
-import App from './App';
+import App from './App'
 // PWA progressive web application
 // https协议的服务器上
 // import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// 帮助我们把组件挂载在某个节点上
+// 组件开头必须以大写字母开头
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // serviceWorker.unregister();
