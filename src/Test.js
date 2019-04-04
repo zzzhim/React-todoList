11371802d62b0ea43bd66cba1d4ebe8b4ae70801
wangyafei <3218566389@qq.com>
2019-04-04 23:05:23 +0800

    React中的虚拟DOM

---
+++ src/Test.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+class Test extends Component {
+    // 当父组件的render函数被运行时，它的子组件的render都将被重新运行
+    render() {
+        return <div>{ this.props.content }</div>
+    }
+}
+
+export default Test
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import TodoItem from './TodoItem'
+import Test from './Test.js'
 import './style.css'
 
 // 定义一个类
@@ -69,6 +70,7 @@ class TodoList extends Component {
         })
     }
 
+    // 当组件的state和props发生改变的时候，render函数就会重新渲染
     render() {
         // JSX
         return (
@@ -87,9 +89,60 @@ class TodoList extends Component {
                 </div>
                 {/* 循环渲染数据 */}
                 <ul>{ this.onTodoItem() }</ul>
+                <Test content={ this.state.inputValue } />
             </Fragment>
         )
     }
 }
 
 export default TodoList
+
+// 1. state 数据
+// 2. JSX 模板
+// 3. 数据 + 模板 结合，生成真实的DOM，替换原始的DOM
+// 4. state 发生改变
+// 5. 数据 + 模板 结合,生成真实的DOM，替换原始的DOM
+
+// 缺陷：
+// 第一次生成了一个完整的DOM片段
+// 第二次生成了一个完整的DOM片段
+// 第二次的DOM替换第一次的DOM，非常耗性能
+
+// 1. state 数据
+// 2. JSX 模板
+// 3. 数据 + 模板 结合，生成真实的DOM，替换原始的DOM
+// 4. state 发生改变
+// 5. 数据 + 模板 结合，生成真实的DOM，并不直接替换原始的DOM
+// 6. 新的DOM 和 原始的DOM做比对，找差异 (DOM和DOM做对比)
+// 7. 找出Input框发生了变化
+// 8. 只用新的DOM中的Input元素，替换掉老的DOM中的input元素
+
+// 缺陷：
+// 性能的提示并不明显
+
+// 1. state 数据
+// 2. JSX 模板
+// 3. 数据 + 模板 结合，生成真实的DOM，替换原始的DOM
+// 4. 生成虚拟DOM(虚拟DOM就是一个JS对象，用它来描述真实DOM) (损耗了性能，但是代价很小)
+// [
+//     'div',
+//     { id: 'abc' },
+//     [
+//         'span',
+//         { class: 'a' },
+//         '我是一个虚拟DOM,是不是和vue的jsx写法很像'
+//     ]
+// ]
+// 5. state 发生变化
+// 6. 数据 + 模板 生成新的虚拟DOM (极大的提升了性能)
+// [
+//     'div',
+//     { id: 'abc' },
+//     [
+//         'span',
+//         { class: 'a' },
+//         '我是新的虚拟DOM，内容发生了变化'
+//     ]
+// ]
+// 7. 比较原始虚拟DOM和新的虚拟DOM的区别，找到区别是span中内容 (JS和JS做对比)
+// 8. 直接操作DOM，改变span中的内容
